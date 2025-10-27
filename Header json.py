386fd7e5453d0e15c40c0e0aa4428c1d5c1fd367
VahidKianiCoding <vahidkianibiz@gmail.com>
2025-10-27 15:49:52 +0330

    Fresh start

--- Header json.py
+++ Header json.py
@@ -6,7 +6,7 @@ headers = {
   'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
   'content-type': 'application/json',
   'priority': 'u=1, i',
-  'referer': 'https://x.com/search?q=(%22Palestine%22)%20(from%3ALauraLoomer)%20until%3A2025-10-27%20since%3A2025-10-10&src=typed_query',
+  'referer': 'https://x.com/search?q=(from%3ALauraLoomer)%20until%3A2025-10-27%20since%3A2025-10-01&src=typed_query',
   'sec-ch-ua': '"Brave";v="141", "Not?A_Brand";v="8", "Chromium";v="141"',
   'sec-ch-ua-mobile': '?0',
   'sec-ch-ua-platform': '"Windows"',
@@ -15,18 +15,18 @@ headers = {
   'sec-fetch-site': 'same-origin',
   'sec-gpc': '1',
   'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36',
-  'x-client-transaction-id': 'yLBI/13M0pVttjZKPoIVBCNxrhlwVb22wcRx17mDgs031UZn5adJCTPMIPHvsogjRNQRZ8wPGsHmoLSFhHinnwv+p3Dbyw',
+  'x-client-transaction-id': 'EWmRJoQVC0y0b++T51vM3fqod8CpjGRvGB2oDmBaWxTuDJ++PH6Q0OoV+Sg2a1H6nZv9vhVm1SJ/nxOLOXYfzu2O3vm6Eg',
   'x-csrf-token': '5ffb38d9dd21ede5c28332ed8c4f2d7e5e9458c20870815daebd2e04ee8f2475d083e8ab0bf7efaf286a6d626222e7d74120ad57329d95449874042b4ad72e909287fc863763a98dd1fed826b1775a9f',
   'x-twitter-active-user': 'yes',
   'x-twitter-auth-type': 'OAuth2Session',
   'x-twitter-client-language': 'en',
-  'x-xp-forwarded-for': '867313f1de232f109745a9bfe48a82921a83ee14cd21585b7f4832fab77978ff691800287186236629e30a9932bcb19c3a5458ae4f6204232e69a1c7ada868721a5f716bb18ced5aed8aecbb86db4333d56eb49b72f419cbf5d2244882a82c841b282a21f5dd0993c456e0511cb3bd4cb0f46850d31ecda3f964d10dcced1760463f4c419e509f67a40f6f9af7268472469352d75e41ed551bb5c4047d30802cf5410f11c090694fa66f72763d2205ee67d8b3fdb96d9ccccaecddf02cfa8225915e738b95328e1763f7ef4b280a0f72b5506d5470bb21a0da2b596e70ce4100eba6e2f356dc4f59f8495598fe90a0e6508307cfb0ad1a32b5071a',
-  'Cookie': 'd_prefs=MjoxLGNvbnNlbnRfdmVyc2lvbjoyLHRleHRfdmVyc2lvbjoxMDAw; __cuid=f4a31d4f32114b23bf07b8d76d88012a; kdt=q934k6Jv4o3OyKUpkUst5puuYNLmPxdnrvKRvD94; g_state={"i_l":0,"i_ll":1761552074589,"i_b":"IG0dpgXbZphDSwZ+7iOPnEBcYVqeWVv8dcdvTTjWgfM"}; dnt=1; auth_multi="814875951498665984:a2022f3a4471541402c729722079b9551af3b586"; auth_token=2646e14ee38bac62ed87f669a761f55a400660da; guest_id=v1%3A176155210269506988; twid=u%3D1591785998702333958; ct0=5ffb38d9dd21ede5c28332ed8c4f2d7e5e9458c20870815daebd2e04ee8f2475d083e8ab0bf7efaf286a6d626222e7d74120ad57329d95449874042b4ad72e909287fc863763a98dd1fed826b1775a9f; lang=en; __cf_bm=zzFn3jFr9ZoDg_u_mKTsdHyO9z.k2twxCTM98lBMLeU-1761557642.5801322-1.0.1.1-I3G72UgXvjfgXuOFmSspkBwtYYW.y0BUSLPQUzh1l8DZtgg3GEbINWdFyjvk79Wr44PA2ZE_t6q4z4oKcWQGG82xcUOC2j7M1NjNWHY8N9Voq5U86ex5FzPlLKIQRzkX'
+  'x-xp-forwarded-for': 'c04e170eb98f573f8cb06ec78978476c8001d75089dd6ed18d918812fef185b2a7d27f63a6cb1107f94573fc55ae2024da30adbbc5454a33d1c10996ec12a5c9b68115413483d238d42db5db7ae1036280276222f2ba6e4c25c5ec1871d23bbe1e00b1d1e2b59b776c276354cf9d75e16bb43274cec91d375a8fe84543100fc9dfda1bdff1d0f073b483cec328a7299b3604b03a34a841460be31622829bc2639e178ed675586949b2e9e69ce0143a725a96c6f19e44ddf48a0a0270382a34e66e17d28792504ce9d85af6424833f638b797525602721b8534869dd47a2667fa69a2ee5ddc08c15ebc35021ade4ccb3c24e56f6ded7f01c55c6e',
+  'Cookie': 'd_prefs=MjoxLGNvbnNlbnRfdmVyc2lvbjoyLHRleHRfdmVyc2lvbjoxMDAw; __cuid=f4a31d4f32114b23bf07b8d76d88012a; kdt=q934k6Jv4o3OyKUpkUst5puuYNLmPxdnrvKRvD94; g_state={"i_l":0,"i_ll":1761552074589,"i_b":"IG0dpgXbZphDSwZ+7iOPnEBcYVqeWVv8dcdvTTjWgfM"}; dnt=1; auth_multi="814875951498665984:a2022f3a4471541402c729722079b9551af3b586"; auth_token=2646e14ee38bac62ed87f669a761f55a400660da; guest_id=v1%3A176155210269506988; twid=u%3D1591785998702333958; ct0=5ffb38d9dd21ede5c28332ed8c4f2d7e5e9458c20870815daebd2e04ee8f2475d083e8ab0bf7efaf286a6d626222e7d74120ad57329d95449874042b4ad72e909287fc863763a98dd1fed826b1775a9f; lang=en; __cf_bm=YJdxfcp9MZ.TFie1S548UrNmkbwK9iiW_rj1UA9r5x0-1761562617.5261493-1.0.1.1-wv6JcmY8lqBDFJyT5gzXGgR5HOVG0L3dq9D4oSd28xkxObyDnbwzH9R60EJijRSHyrrBbK0wH13cTGf_rbVr.ToijYXps8sBUn9t7RcQ2GWhlGBI1jtPo2szqiind20V; __cf_bm=cmLwNXIMQdcDHdOT5hl5hRaAm7_1m56BFU.1RSrm2wo-1761562673.624953-1.0.1.1-RyfCXFLGZKy25iVMIGSNxpl8Q8caitzFjlNl_0EAC1p5BemopPOKk7J45ir_afOFfKqzz5A2.S9qOHflGJBtk3AIKlH1EP06URri30FQTl3XByuvKDLW84NEdcKE4zYo'
 }
 
-headers_json = json.dumps(headers)
+header_json = json.dumps(headers)
 
-print(headers_json)
+print(header_json)
 
-with open("headers.json", "w") as heardersJSON:
-  json.dump(headers_json, heardersJSON)
+with open("header.json", "w") as headersJSON:
+  json.dump(header_json, headersJSON)
